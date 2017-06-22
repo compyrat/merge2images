255a6e47c3be263b88a3fe6b5bd0485417a434ae
Albert Marlet <albertmarlet@Alberts-MacBook-Pro.local>
2017-06-22 11:42:39 +0200

    Fix name

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.merge = function(bigImg, smallImg, outImg){
 	images(bigImg).draw(images(smallImg), 0, 0).save(outImg);
 }
 
-exports.mergeWhitCoordinates = function(bigImg, smallImg, outImg, x, y){
+exports.mergeWithCoordinates = function(bigImg, smallImg, outImg, x, y){
 	images(bigImg).draw(images(smallImg), x, y).save(outImg);
 }
