5253b96ad5eb27f7809e5c6a8a78553020a6faf4
Albert Marlet <albertmarlet@Alberts-MacBook-Pro.local>
2017-06-22 12:33:35 +0200

    add new functions and modify READMDE

--- README.md
+++ README.md
@@ -8,3 +8,26 @@ Merge images using the api of [Images](https://www.npmjs.com/package/images)
 ```sh
 $ npm install merge2images
 ```
+
+# API
+```javascript
+const images = require('merge2images');
+
+//merge big image with small image and save at out image
+images.merge(bigImg, smallImg, outImg);
+
+//merge big image with small image at coordinates (x,y) and save at out image
+images.mergeWithCoordinates(bigImg, smallImg, outImg, x, y);
+
+//merge big image with small image, save at out image and auto scaling with number of width pixel
+images.mergeWithAutoScaling(bigImg, size1, smallImg, size2, outImg);
+
+//merge big image with small image, save at out image and manual scaling with number of width and height pixels
+images.mergeWithManualScaling(bigImg, width1, height1, smallImg, width2, height2, outImg);
+
+//merge big image with small image, save at out image, manual scaling and set coordinates(x,y)
+images.mergeWithAll(bigImg, width1, height1, smallImg, width2, height2, outImg, x, y);
+
+//merge big image with small image, save at out image, manual scaling and set small image at the center of the screen
+images.mergeWithAllCenter(bigImg, width1, height1, smallImg, width2, height2, outImg);
+```
--- index.js
+++ index.js
@@ -7,3 +7,30 @@ exports.merge = function(bigImg, smallImg, outImg){
 exports.mergeWithCoordinates = function(bigImg, smallImg, outImg, x, y){
 	images(bigImg).draw(images(smallImg), x, y).save(outImg);
 }
+
+exports.mergeWithAutoScaling = function(bigImg, size1, smallImg, size2, outImg){
+	images(bigImg).size(size1).draw(images(smallImg).size(size2), 0, 0).save(outImg);
+}
+
+exports.mergeWithManualScaling = function(bigImg, width1, height1, smallImg, width2, height2, outImg){
+	images(bigImg).size(width1).height(height1).draw(images(smallImg).size(width2).height(height2), 0, 0).save(outImg);
+}
+
+exports.mergeWithAll = function(bigImg, width1, height1, smallImg, width2, height2, outImg, x, y){
+	images(bigImg).size(width1).height(height1).draw(images(smallImg).size(width2).height(height2), x, y).save(outImg);
+}
+
+exports.mergeWithAllCenter = function(bigImg, width1, height1, smallImg, width2, height2, outImg){
+	if (width1 >= width2){
+		var x = (width1/2) - (width2/2);
+	}else{
+		var x = 0;
+	}
+
+	if (height1 >= height2){
+		var y = (height1/2) - (height2/2);
+	}else{
+		var y = 0;
+	}
+	images(bigImg).size(width1).height(height1).draw(images(smallImg).size(width2).height(height2), x, y).save(outImg);
+}
